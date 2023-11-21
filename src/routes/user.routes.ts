cd3765ac27ec7022b84092896e930641e73abce3
emersonHP <HP@DESKTOP-4FK04Q7>
2023-11-21 15:37:24 -0600

    user auth

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,17 +2,18 @@ import { User } from './../models/User.1';
 import { Router } from "express";
 import UserController from "../controllers/User.controller";
 import bcrypt from 'bcrypt'
-const salround =10;
+import { checkToken } from '../jwtvalidation/jwt.validation';
+
 
 
 const router = Router()
 const user = UserController
 
-router.get('/', user.listUsers)
-router.post('/', user.createUser)
-router.get('/:id', user.byIdUser)
-router.put('/:id', user.updateUser)
-router.delete('/:id', user.deleteUser)
+router.get('/', checkToken, user.listUsers)
+router.post('/', checkToken, user.createUser)
+router.get('/:id', checkToken, user.byIdUser)
+router.put('/:id', checkToken, user.updateUser)
+router.delete('/:id',checkToken, user.deleteUser)
 
 
 export default router
